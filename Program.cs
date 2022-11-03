0e4d7ca000cf08fb450a6ec5cbc97fc552327090
Aghayev0 <tu6k6wzh2@code.edu.az>
2022-11-04 01:26:04 +0400

    muellim xeste idim deye anca bu qeder bacardim

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿{
+    int[] item = test(new[] { 1, 5, 7, 9, 11, 13 });
+    Console.Write("Ilk ve sonuncunu swap eliyende: ");
+    foreach (var i in item)
+    {
+        Console.Write(i.ToString() + " ");
+    }
+}
+static int[] test(int[] numbers)
+{
+    int first = numbers[0];
+    numbers[0] = numbers[numbers.Length - 1];
+    numbers[numbers.Length - 1] = first;
+
+    return numbers;
+}
